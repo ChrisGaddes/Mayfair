723ddbf36aaf4bee59499266d73c962b5f5c1548
Chris Gaddes <cagaddes@gmail.com>
2019-10-05 13:57:37 -0500

    Initial commit

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mayfair/app/home_page.dart';
+import 'package:mayfair/app/sign_in/sign_in_page.dart';
+import 'package:mayfair/services/auth.dart';
+import 'package:provider/provider.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context);
+    return StreamBuilder<User>(
+        stream: auth.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            User user = snapshot.data;
+            if (user == null) {
+              return SignInPage.create(context);
+            }
+            return HomePage();
+          } else {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mayfair/app/landing_page.dart';
+import 'package:mayfair/services/auth.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider<AuthBase>(
+      builder: (context) => Auth(),
+      child: MaterialApp(
+        title: 'Mayfair',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: LandingPage(),
+      ),
+    );
+  }
+}
